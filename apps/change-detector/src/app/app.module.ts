b1f7d299d4ea58e076f89ec76fbd210ddfcd7e0a
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-05-19 15:48:08 -0500

    adding shell component

--- apps/change-detector/src/app/app.module.ts
+++ apps/change-detector/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChangeDetectorShellModule} from '@ka/change-detector/shell';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ChangeDetectorShellModule],
   providers: [],
   bootstrap: [AppComponent]
 })
