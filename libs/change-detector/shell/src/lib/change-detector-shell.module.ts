997ca3a721ae38f7827976f7c8d6be7b1c47ef2e
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-05-19 15:31:24 -0500

    adding nav
    ng generate @angular/material:nav navigation --project=change-detector-shell
    --prefix=cd --styleext=scss --selector=cd-nav
    --module=change-detector-shell.module.ts --entryComponent

--- libs/change-detector/shell/src/lib/change-detector-shell.module.ts
+++ libs/change-detector/shell/src/lib/change-detector-shell.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   imports: [
@@ -8,7 +11,21 @@ import { RouterModule } from '@angular/router';
 
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ])
-  ]
+    ]),
+
+    LayoutModule,
+
+    MatToolbarModule,
+
+    MatButtonModule,
+
+    MatSidenavModule,
+
+    MatIconModule,
+
+    MatListModule
+  ],
+  declarations: [NavigationComponent],
+  entryComponents: [NavigationComponent]
 })
 export class ChangeDetectorShellModule {}
