410eaaf7e7509af589ceef619ef3ec50a21453c4
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-05-19 19:20:32 -0500

    color block comp
    ng generate @schematics/angular:component color-block --project=common-ui
    --module=common-ui.module.ts --export --prefix=ui --styleext=scss --style=scss
    --selector=ui-color-block

---
+++ libs/common/ui/src/lib/color-block/color-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-color-block',
+  templateUrl: './color-block.component.html',
+  styleUrls: ['./color-block.component.scss']
+})
+export class ColorBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/common/ui/src/lib/common-ui.module.ts
+++ libs/common/ui/src/lib/common-ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ColorBlockComponent } from './color-block/color-block.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ColorBlockComponent],
+  exports: [ColorBlockComponent]
 })
 export class CommonUiModule {}
